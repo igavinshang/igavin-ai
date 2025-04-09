ff6b05b571b9af1d3a44555c83af78649868656f
igavinshang <igavinshang@qq.com>
2025-04-09 16:58:24 +0800

    清理无用code

--- src/main/java/org/igavin/ai/langchain4j/chat/AliyunChatService.java
+++ src/main/java/org/igavin/ai/langchain4j/chat/AliyunChatService.java
@@ -5,6 +5,7 @@ import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
 import org.igavin.ai.config.AIProperties;
 
 import java.util.List;
@@ -21,25 +22,15 @@ public class AliyunChatService implements ChatService {
 
     @Override
     public String chat(String message) {
-
-        // ToolSpecification toolSpecification = ToolSpecification.builder()
-        // .name("getWeatherServiceFunction")
-        // .description("Use api.weather to get weather information.")
-        // .parameters(JsonObjectSchema.builder()
-        // .addStringProperty("city", "The city for which the weather forecast should be
-        // returned")
-        // .required("city") // the required properties should be specified explicitly
-        // .build())
-        // .build();
-
         List<ChatMessage> chatMessages = List.of(
                 new SystemMessage("You are a helpful AI assistant."),
                 new UserMessage(message));
-        // ChatRequest chatRequest = ChatRequest.builder()
-        // // .toolSpecifications(toolSpecification)
-        // .messages(chatMessages)
-        // .build();
 
-        return chatModel.generate(chatMessages).content().text().toString();
+        ChatRequest chatRequest = ChatRequest.builder()
+                .messages(chatMessages)
+                .build();
+
+        return chatModel.chat(chatRequest).aiMessage().text();
     }
+
 }
